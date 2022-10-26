54ad47f3c7dff1533e45b17d5b5266923d47d812
Gamearoo Codes <gamearoo10@gmail.com>
2022-10-26 16:54:22 -0400

    t

--- src/Commands/Embeds/ourad.js
+++ src/Commands/Embeds/ourad.js
@@ -1,26 +1,33 @@
-const { PermissionFlagsBits, EmbedBuilder, CommandInteraction, Client } = require("discord.js");
-const fs = require('node:fs');
+const {
+  PermissionFlagsBits,
+  EmbedBuilder,
+  CommandInteraction,
+  Client,
+} = require("discord.js");
+const fs = require("node:fs");
 const path = require("node:path");
 
 module.exports = {
-    name: 'embed',
-    description: 'the ad embed',
-    perm: PermissionFlagsBits.Administrator,
-    /**
-     * 
-     * @param {CommandInteraction} interaction 
-     * @param {Client} client 
-     * @param {*} extras 
-     */
-    async run(interaction, client, extras) {
-        let pathtxt = path.join(__dirname, '../../../text docs/ourad.txt');
-        var text = fs.readFileSync(pathtxt).toString('utf-8');
+  name: "embed",
+  description: "the ad embed",
+  perm: PermissionFlagsBits.Administrator,
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   * @param {*} extras
+   */
+  async run(interaction, client, extras) {
+    let pathtxt = path.join(__dirname, "../../../text docs/ourad.txt");
+    var text = fs.readFileSync(pathtxt).toString("utf-8");
 
-        const embed = new EmbedBuilder().setDescription(text).setTitle('Ram Bot v14.1.x pre Notes').setColor("DarkVividPink");
+    const embed = new EmbedBuilder()
+      .setDescription(text)
+      .setTitle("Ram Api  v11 Pre-Notes")
+      .setColor("DarkVividPink");
 
-        interaction.channel.send({ embeds: [embed] });
+    interaction.channel.send({ embeds: [embed] });
 
-        interaction.reply({ content: 'Done', ephemeral: true })
-
-    }
-}
+    interaction.reply({ content: "Done", ephemeral: true });
+  },
+};
